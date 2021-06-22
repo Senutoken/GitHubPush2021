e63377c8bed383a84e48560fb49448d631377bc8
Senutoken <temporalcrux@gmail.com>
2021-06-21 23:01:08 -0400

    Initial commit in 2021!

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+        System.out.println("My first push to GitHub in 2021");
+    }
+}
