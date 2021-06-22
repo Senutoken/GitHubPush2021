8258ccd612a9e70ead6b572b1ec0b09ff2915807
Senutoken <temporalcrux@gmail.com>
2021-06-21 23:01:55 -0400

    2nd commit!

--- src/Main.java
+++ src/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("My first push to GitHub in 2021");
+        System.out.println("Just pushed! Making an update!");
     }
 }
